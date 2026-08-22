e3ef2d2d6869863bc0d06255e2a1434c5e19bd77
Benigno Fanjul <beni@iMac-de-Benigno.local>
2026-08-22 18:49:13 +0200

    Prueba de geolocalización

--- app.py
+++ app.py
@@ -110,6 +110,8 @@ if tipo_origen == "Mi ubicación actual":
 
     ubicacion = streamlit_geolocation()
 
+    st.write("DEBUG ubicación:", ubicacion)
+
     if ubicacion and ubicacion.get("latitude") is not None:
 
         st.success(
